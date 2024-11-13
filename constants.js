3a6b126a4e3d834c91ffe8e9ba54263ffb599fe0
DihyahAdib <112100886+DihyahAdib@users.noreply.github.com>
2024-11-13 01:05:37 -0500

    ncie

--- constants.js
+++ constants.js
@@ -1,40 +1,14 @@
 // constants.js
-export const $ = (selector) => {
-  return document.querySelector(selector);
-};
 
-export const $$ = (selector) => {
-  return document.querySelectorAll(selector);
-};
-
-export const buttons = {
-  input: document.querySelector("#input-vocab"),
-  removeInput: document.querySelector("#remove-vocab"),
-  removeOne: document.querySelector("#remove-one"),
-  pickMode: document.getElementById("pick-mode"),
-};
-
-export const elements = {
-  textArea: document.querySelector("#text-area"),
-  definitionArea: document.querySelector("#definition-area"),
-  wordsContainer: document.querySelector("#words-container"),
-  modal: document.querySelector(".modal"),
-  modalExplain: document.querySelector(".modal-explanation"),
-  overlay: document.getElementById("overlay"),
-  closeModal: document.getElementById("close-modal"),
-  closeExplainbtn: document.getElementById("close-modal-explanation"),
-  modeNameElement: document.getElementById("mode-name"),
-  currentMode: document.querySelector(".current-mode"),
-  currentModeExplain: document.getElementById("current-mode-explain-name"),
-  explainArea: document.getElementById("explaination-area")
-};
+// currentMode: $(".current-mode")
 
 export const currentModeName = [
     {name: "Cover Mode", Description: 'This mode will allow you to only show the "Foriegn language," so it will show just the vocabulary and not its definition. This is not to be confused with Flash card mode.', color: "green"},
 
     {name: "Casual Mode", Description: "Casual mode has no timer and just allows you to quiz yourself on your own time, but the feature for choosing which words to include in your test still stands.", color: "orange"},
 
-    {name: "Timed Mode", Description: "Casual & Timed Flash Card Mode: This mode has a few qualities. First, there are two modes to this mode: timer and casual. Timer mode will start a clock and randomly randomize the words from your word bank, effectively quizzing you from the words you've inputted. You're allowed to select a certain word for this mode, or just select all of the words in your bank if you're up for the challenge! The word will appear on the screen, and you'll be prompted to give the correct definition; if it's wrong, it will flip and reveal the right answer.", color: "blue"},
+    {name: "Timed Mode", Description: "The Casual & Timed Flash Card Mode offers two modes: timer and casual. Timer mode randomly randomizes words from your word bank, quizzing you. You can select a word or all words, with the word appearing on the screen and correct definition provided.", color: "blue"},
 
     {name: "Memorization Mode", Description: "..." , color: "red"}
 ];
+
--- script.js
+++ script.js
@@ -1,160 +1,175 @@
 // script.js
-import { buttons, elements, $, $$, currentModeName } from "./constants.js";
+import { currentModeName } from "./constants.js";
 
 let storeWords = JSON.parse(localStorage.getItem("storeWords")) || [];
 
+$("#input-vocab").on("click", addWords);
+$("#cover").on("click", coverMode);
+$("#casual").on("click", casualMode);
+$("#timed").on("click", timedMode);
+$("#memo-mode").on("click", memorizationMode);
+
 function displayWords() {
-  elements.wordsContainer.innerHTML = "";
+  $("#words-container").empty();
 
   storeWords.forEach(({ word, definition }) => {
-    const wordbutton = document.createElement("button");
-    wordbutton.textContent = `${word} : ${definition}`;
-    wordbutton.dataset.word = word;
-    wordbutton.dataset.definition = definition;
-    wordbutton.onclick = function() { //remove itself
+    $("<button>")
+      .addClass("new-element")
+      .text(`${word} : ${definition}`)
+      .on("click", function() {
         this.remove();
-        storeWords = storeWords.filter(function({word: storedWord, definition: storedDefinition}) {
-            return !(word === storedWord && definition === storedDefinition)
+        storeWords = storeWords.filter(function ({
+          word: storedWord,
+          definition: storedDefinition,
+        }) {
+          return !(word === storedWord && definition === storedDefinition);
         });
         save();
-    };
-
-    wordbutton.style.fontWeight = 'bold';
-    wordbutton.style.fontSize = '26px';
-    elements.wordsContainer.appendChild(wordbutton);
-    wordbutton.classList.add("new-element");
+      })
+      .appendTo("#words-container")
+      .data({ word, definition });
   });
-};
+}
 
-buttons.input.addEventListener("click", function () {
-  const word = elements.textArea.value.trim();
-  const definition = elements.definitionArea.value.trim();
+function addWords() {
+  const word = $("#text-area").val().trim();
+  const definition = $("#definition-area").val().trim();
 
   if (word && definition) {
     storeWords.push({ word, definition });
-    save();
-    elements.textArea.value = "";
-    elements.definitionArea.value = "";
+    $("#text-area").val("");
+    $("#definition-area").val("");
     displayWords();
+    save();
   } else {
     alert("Enter both the vocab & definition first!");
   }
+}
+
+$("text-areas input").on("keypress", function (e) {
+  if (e.key === "Enter") {
+    addWords();
+  }
 });
 
-buttons.removeInput.addEventListener("click", function () {
+$("#remove-vocab").on("click", function () {
   storeWords.length = 0;
   localStorage.removeItem("storeWords");
   displayWords();
 });
 
-buttons.removeOne.addEventListener("click", function () {
+$("#remove-one").on("click", function () {
   storeWords.pop();
-  save();
   displayWords();
+  save();
 });
 
-buttons.pickMode.addEventListener("click", function() {
-  elements.overlay.style.display = "flex";
-  elements.overlay.style.animation = "opacityI 0.68s forwards";
-  elements.modal.style.animation = "scaleIn 0.68s forwards";
+$("#pick-mode").on("click", function () {
+  $("#overlay").css({ display: "flex", animation: "opacityI 0.68s forwards" });
+  $(".modal").css({ animation: "scaleIn 0.68s forwards" });
 });
 
-elements.closeModal.addEventListener("click", function() {
-  elements.modal.style.animation = "scaleOut 0.58s forwards";
-  elements.overlay.style.animation = "opacityO 0.5s forwards";
-  
-  elements.modal.addEventListener("animationend", function handleAnimationEnd() {
-    elements.overlay.style.display = "none";
-    elements.modal.removeEventListener("animationend", handleAnimationEnd);
+$("#close-modal").on("click", function () {
+  $("#overlay").css({ animation: "opacityO 0.5s forwards" });
+
+  $(".modal")
+    .css({ animation: "scaleOut 0.58s forwards" })
+    .on("animationend", function handleAnimationEnd() {
+      $("#overlay").css({ display: "none" });
+      $(".modal").off("animationend", handleAnimationEnd);
+    });
+});
+
+$("#close-modal-explanation").on("click", function () {
+  $(".modal-explanation").css({ animation: "slideOut 0.6s forwards" });
+  $(".modal").on("animationend", function handleAnimationEnd() {
+    $(".modal").off("animationend", handleAnimationEnd);
   });
 });
 
-elements.closeExplainbtn.addEventListener("click", function() {
-  elements.modalExplain.style.animation = "slideOut 0.6s forwards";
-  elements.modal.addEventListener("animationend", function handleAnimationEnd() {
-    elements.modal.removeEventListener("animationend", handleAnimationEnd);
+$("#cover").on("mouseover", function () {
+  $(".modal-explanation").css({
+    visibility: "visible",
+    animation: "slideIn 0.6s forwards",
   });
+  $("#current-mode-explain-name")
+    .text(currentModeName[0].name)
+    .css({ color: currentModeName[0].color });
+  $("#explaination-area").text(currentModeName[0].Description);
 });
 
-$("#cover").onclick = () => coverMode();
-$("#casual").onclick = () => casualMode();
-$("#timed").onclick = () => timedMode();
-$("#memo-mode").onclick = () => memorizationMode();
-
-$("#cover").addEventListener("mouseover", function() {
-  elements.modalExplain.style.visibility = "visible";
-  elements.modalExplain.style.animation = "slideIn 0.6s forwards";
-  elements.currentModeExplain.innerText = currentModeName[0].name;
-  elements.currentModeExplain.style.color = currentModeName[0].color;
-  elements.explainArea.innerText = currentModeName[0].Description;
+$("#casual").on("mouseover", function () {
+  $(".modal-explanation").css({
+    visibility: "visible",
+    animation: "slideIn 0.6s forwards",
+  });
+  $("#current-mode-explain-name")
+    .text(currentModeName[1].name)
+    .css({ color: currentModeName[1].color });
+  $("#explaination-area").text(currentModeName[1].Description);
 });
 
-$("#casual").addEventListener("mouseover", function() {
-  elements.modalExplain.style.visibility = "visible";
-  elements.modalExplain.style.animation = "slideIn 0.6s forwards";
-  elements.currentModeExplain.innerText = currentModeName[1].name;
-  elements.currentModeExplain.style.color = currentModeName[1].color;
-  elements.explainArea.innerText = currentModeName[1].Description;
+$("#timed").on("mouseover", function () {
+  $(".modal-explanation").css({
+    visibility: "visible",
+    animation: "slideIn 0.6s forwards",
+  });
+  $("#current-mode-explain-name")
+    .text(currentModeName[2].name)
+    .css({ color: currentModeName[2].color });
+  $("#explaination-area").text(currentModeName[2].Description);
 });
 
-$("#timed").addEventListener("mouseover", function() {
-  elements.modalExplain.style.visibility = "visible";
-  elements.modalExplain.style.animation = "slideIn 0.6s forwards";
-  elements.currentModeExplain.innerText = currentModeName[2].name;
-  elements.currentModeExplain.style.color = currentModeName[2].color;
-  elements.explainArea.innerText = currentModeName[2].Description;
-});
-
-$("#memo-mode").addEventListener("mouseover", function() {
-  elements.modalExplain.style.visibility = "visible";
-  elements.modalExplain.style.animation = "slideIn 0.6s forwards";
-  elements.currentModeExplain.innerText = currentModeName[3].name;
-  elements.currentModeExplain.style.color = currentModeName[3].color;
-  elements.explainArea.innerText = currentModeName[3].Description;
+$("#memo-mode").on("mouseover", function () {
+  $(".modal-explanation").css({
+    visibility: "visible",
+    animation: "slideIn 0.6s forwards",
+  });
+  $("#current-mode-explain-name")
+    .text(currentModeName[3].name)
+    .css({ color: currentModeName[3].color });
+  $("#explaination-area").text(currentModeName[3].Description);
 });
 
 export function coverMode() {
-  const wordButtons = elements.wordsContainer.querySelectorAll("button.new-element");
-  wordButtons.forEach(button => {
-    const word = button.dataset.word;
-    button.textContent = `${word}`;
-    save();
+  $("button.new-element").each(() => {
+    console.log($(this).data("word"));
+    $(this).text($(this).data("word"));
   });
-
-  elements.modeNameElement.innerText = currentModeName[0].name;
-  elements.modeNameElement.style.color = currentModeName[0].color;
+  
+  $("#mode-name")
+    .text(currentModeName[0].name)
+    .css({ color: currentModeName[0].color });
   save();
-};
+}
 
 export function casualMode() {
-  const wordButtons = elements.wordsContainer.querySelectorAll("button.new-element");
-  elements.modeNameElement.innerText = currentModeName[1].name;
-  elements.modeNameElement.style.color = currentModeName[1].color;
+  $("#mode-name")
+    .text(currentModeName[1].name)
+    .css({ color: currentModeName[1].color });
 
-  wordButtons.forEach(button => {
-    const word = button.dataset.word;
-    const definition = button.dataset.definition;
-    button.textContent = `${word} : ${definition}`;
-    save();
+  $("button.new-element").each(() => {
+    $(this).text(`${$(this).data("word")} : ${$(this).data("definition")}`);
   });
-};
+  save();
+}
 
 export function timedMode() {
-
-  elements.modeNameElement.innerText = currentModeName[2].name;
-  elements.modeNameElement.style.color = currentModeName[2].color;
+  $("#mode-name")
+    .text(currentModeName[2].name)
+    .css({ color: currentModeName[2].color });
   save();
-};
+}
 
 export function memorizationMode() {
-
-  elements.modeNameElement.innerText = currentModeName[3].name;
-  elements.modeNameElement.style.color = currentModeName[3].color;
+  $("#mode-name")
+    .text(currentModeName[3].name)
+    .css({ color: currentModeName[3].color });
   save();
-};
+}
 
 displayWords();
 
 function save() {
-    localStorage.setItem("storeWords", JSON.stringify(storeWords));
-};
+  localStorage.setItem("storeWords", JSON.stringify(storeWords));
+}
