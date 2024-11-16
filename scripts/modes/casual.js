9e495069fc0bed99762b4cbf32dc1e0c250c30b4
DihyahAdib <112100886+DihyahAdib@users.noreply.github.com>
2024-11-16 16:36:59 -0500

    working carousel

--- scripts/modes/casual.js
+++ scripts/modes/casual.js
@@ -3,6 +3,41 @@
 import { randomizeArray } from "../functions/wordhandler.js";
 import { save } from "../save.js";
 
+let cardIndex = 1;
+showCards(cardIndex);
+
+export function plusCards(n) {
+  showCards((cardIndex += n));
+}
+
+export function currentCards(n) {
+  showSlides((slideIndex = n));
+}
+
+export function showCards(n) {
+  let cards = $(".flash-card-object");
+  let dots = $(".dot");
+  if (n > cards.length) {
+    cardIndex = 1;
+  }
+  if (n < 1) {
+    cardIndex = cards.length;
+  }
+  cards.css("display", "none");
+
+  // Remove "active" class from all dots
+  dots.removeClass("active");
+
+  // Show the current slide
+  cards.eq(cardIndex - 1).css("display", "block");
+
+  // Add "active" class to the corresponding dot
+  dots.eq(cardIndex - 1).addClass("active");
+}
+
+window.plusCards = plusCards;
+window.currentCards = currentCards;
+
 export async function casualMode() {
   $("button.new-element").each(function () {
     $(this).text(`${$(this).data("word")} : ${$(this).data("definition")}`);
@@ -10,10 +45,11 @@ export async function casualMode() {
   randomizeArray(storeWords);
 
   storeWords.forEach(({ word, definition }) => {
-    $("<button>")
-      .addClass("flash-card-object")
+    $("<div>")
+      .addClass("flash-card-object mySlides")
       .html(`${word}: ${definition}`)
-      .appendTo("modal-container");
+      .prependTo("flash-card-container");
+    // $("<span>").addClass("dot").apendTo("dot-container");
   });
   save();
 }
