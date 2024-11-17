4e32751bb2a55d53337820ef470006a36c7d941f
DihyahAdib <112100886+DihyahAdib@users.noreply.github.com>
2024-11-16 19:56:36 -0500

    colors

--- scripts/events.js
+++ scripts/events.js
@@ -14,6 +14,8 @@ import { addWords } from "./functions/wordhandler.js";
 import { setCurrentMode } from "./constants.js";
 import { coverMode } from "./modes/cover.js";
 import { casualMode, toggleDisplayGrid } from "./modes/casual.js";
+import { timedMode } from "./modes/timed.js";
+import { memorizationMode } from "./modes/memorization.js";
 
 export async function wait(ms) {
   await new Promise((resolve) => setTimeout(resolve, ms));
@@ -28,14 +30,19 @@ $("#memo-mode").on("click mouseover", () => setCurrentMode(3));
 // Click | Press Events //
 $("#cover").on("click", coverMode);
 $("#casual").on("click", casualMode);
+$("#timed").on("click", timedMode);
+$("#memo-mode").on("click", memorizationMode);
+
 $("#input-vocab").on("click", addWords);
 $("#inputbutton").on("click", toggleDisplayGrid);
 $("#pick-mode").on("click", function () {
   showModal();
 });
+
 $("#close-modal").on("click", function () {
   hideModal();
 });
+
 $("#casual, #timed, #memo-mode")
   .on("mouseover", function () {
     showExplanation();
@@ -46,15 +53,24 @@ $("#casual, #timed, #memo-mode")
       .addClass("modal-play-mode");
   });
 
+$("button-close-flash-cards").on("click", function () {
+  $("body")
+    .addClass("modal-pick-mode modal-pick-mode-explain")
+    .removeClass("modal-play-mode");
+});
+
 $("#close-modal-explanation").on("click", function () {
   hideExplanation();
 });
+
 $("text-areas input").on("keypress", function (e) {
   AddVocab(e);
 });
+
 $("#remove-one").on("click", function () {
   removeOneVocab();
 });
+
 $("#remove-vocab").on("click", function () {
   removeAllVocab();
 });
