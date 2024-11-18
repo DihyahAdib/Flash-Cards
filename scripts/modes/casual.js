4f70f1570a9afe47ecdf1090de18cfb944457238
DihyahAdib <112100886+DihyahAdib@users.noreply.github.com>
2024-11-18 06:23:43 -0500

    YESSIR FINALLY GOT IT WORKING

--- scripts/modes/casual.js
+++ scripts/modes/casual.js
@@ -7,20 +7,27 @@ import {
 } from "../functions/wordhandler.js";
 import { save } from "../save.js";
 
-$(".prev").on("click", () => plusCards(-1));
-$(".next").on("click", () => plusCards(1));
+$(".next").on("click", async function () {
+  await wait(140);
+  plusCards(1);
+  right();
+});
+
+$(".prev").on("click", async function () {
+  await wait(140);
+  plusCards(-1);
+  left();
+});
 
 window.cardIndex = 1;
 window.plusCards = plusCards;
 window.currentCards = currentCards;
 
 export async function plusCards(n) {
-  await wait(140); //some delay cuz ppl can spam
   showCards((cardIndex += n));
 }
 
 export async function currentCards(n) {
-  await wait(140);
   showCards((cardIndex = n));
 }
 
@@ -36,27 +43,30 @@ export async function showCards(n) {
   cards
     .eq(cardIndex - 1) //current card
     .css("display", "grid");
-  //leave first? // ok this anim handles left direction
-  cards.eq(cardIndex - 1).addClass("slideOutToLeft"); //current card
-  await wait(500);
-  cards.removeClass("slideOutToLeft");
-  //come back around, im so confused.
-  cards.eq(cardIndex - 1).addClass("slideInFromRight");
-  await wait(500);
-  cards.removeClass("slideInFromRight");
+  checkContainerStyle();
+}
 
-  //right direction
+async function right() {
+  let cards = $(".flash-card-object");
+  plusCards(1);
   cards.eq(cardIndex - 1).addClass("slideOutToRight");
   await wait(500);
   cards.removeClass("slideOutToRight");
   cards.eq(cardIndex - 1).addClass("slideInFromLeft"); //current card
   await wait(500);
   cards.removeClass("slideInFromLeft");
-
-  checkContainerStyle();
 }
-function right() {}
-function left() {}
+
+async function left() {
+  let cards = $(".flash-card-object");
+  plusCards(-1);
+  cards.eq(cardIndex - 1).addClass("slideOutToLeft"); //current card
+  await wait(500);
+  cards.removeClass("slideOutToLeft");
+  cards.eq(cardIndex - 1).addClass("slideInFromRight");
+  await wait(500);
+  cards.removeClass("slideInFromRight");
+}
 
 export async function casualMode() {
   $("div.flash-card-object").each(function () {
